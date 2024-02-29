4d81ceb15d49c5cadcad3155da24e826c0a3001d
Tejas Darode <123544186+tejasdarode17@users.noreply.github.com>
2024-02-29 15:03:31 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,103 @@
+function myApp() {
+    const key = "-zz2epZRuVEx0wp424GJnE8mi1Oq62pJJHDR-5V6w-8"
+    const resultContainer = document.querySelector(".search-result");
+    const input = document.querySelector(".input-box");
+    const searchButton = document.querySelector(".search");
+    const show = document.querySelector(".show");
+
+    let keyword = "";
+    let page = "";
+
+    async function searchImages() {
+        keyword = input.value;
+        const url = `https://api.unsplash.com/search/photos?page=${page}&query=${keyword}&client_id=${key}&per_page=12`;
+
+        const response = await fetch(url);
+        const data = await response.json();
+
+        const results = data.results;
+
+        if (page === 1) {
+            resultContainer.innerHTML = ""
+        }
+
+        results.map((result) => {
+
+            const image = document.createElement("img");
+            image.src = result.urls.small;
+            const imgLink = document.createElement("a");
+            imgLink.herf = result.links.html;
+            imgLink.target = "_blank"
+
+            imgLink.appendChild(image);
+            resultContainer.appendChild(imgLink);
+
+            show.style.display = "block"
+        })
+
+    }
+
+    searchButton.addEventListener("click", (e) => {
+        page = 1;
+        searchImages();
+        gsap.from(".search-result", {
+            opacity: 0,
+            duration: 3,
+        })
+    }, false);
+
+    show.addEventListener("click", (e) => {
+        page++;
+        searchImages()
+        window.scrollTo({
+            top: document.body.scrollHeight,
+            behavior:"auto"
+        });
+        gsap.from(".search-result", {
+            opacity: 0,
+            duration: 3,
+        });
+    })
+
+}
+myApp()
+
+gsap.from(".logo, .search-form", {
+    opacity: 0,
+    duration: 3,
+})
+
+gsap.from(".show", {
+    y: -50,
+    repeat: -1,
+    yoyo: "true"
+})
+
+
+const scroller = new LocomotiveScroll({
+    el: document.querySelector('[data-scroll-container]'),
+    smooth: true
+})
+
+gsap.registerPlugin(ScrollTrigger)
+
+
+scroller.on('scroll', ScrollTrigger.update)
+
+ScrollTrigger.scrollerProxy(
+    '.container', {
+        scrollTop(value) {
+            return arguments.length ?
+            scroller.scrollTo(value, 0, 0) :
+            scroller.scroll.instance.scroll.y
+        },
+        getBoundingClientRect() {
+            return {
+                left: 0, top: 0, 
+                width: window.innerWidth,
+                height: window.innerHeight
+            }
+        }
+    }
+)
+
